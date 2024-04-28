857cd315be959e8e626237fe68a3ea4b3aeefac7
gabrielabutnaru <gabrielabutnaru24@gmail.com>
2024-04-28 23:06:43 +0300

    👌 Add /login and /me endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import { userRouter } from './routes/userRouter';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(express.json());
+app.use(bodyParser.json());
+app.use('/user', userRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
