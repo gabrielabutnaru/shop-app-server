fae9cae6f8ee7f23f285b8d86f016ea1e3d17edd
gabrielabutnaru <gabrielabutnaru24@gmail.com>
2024-05-02 14:16:43 +0300

    Create register endpoint

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -4,3 +4,11 @@ export const userLoginSchema = z.object({
   username: z.string(),
   password: z.string(),
 });
+
+export const userRegisterSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  username: z.string(),
+  password: z.string(),
+});
